8efca68e035943e07bda71a8759b38451644abb5
jiacheng <jiacheng.li@shanbay.com>
2017-01-12 22:36:53 +0800

    sync

--- ProgramModel/apps.py
+++ ProgramModel/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 
 
 class ProgramConfig(AppConfig):
-    name = 'Program'
+    name = 'ProgramModel'
--- ylive/settings.py
+++ ylive/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'AnchorModel',
-    'Program',
+    'ProgramModel',
 ]
 
 MIDDLEWARE = [
